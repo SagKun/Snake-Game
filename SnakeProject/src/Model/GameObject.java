6384305c016d7c98d876900910b4af17b4d28bc6
oryagol <oryagol@gmail.com>
2019-11-24 19:08:30 +0200

    creating GameObject class content

--- SnakeProject/src/Model/GameObject.java
+++ SnakeProject/src/Model/GameObject.java
@@ -1,5 +1,31 @@
 package Model;
 
-public class GameObject {
+public abstract class GameObject {
+	public static final int SIZE = 20;
+	protected int X, Y;
+	
+	public GameObject(int x, int y){
+		
+		this.X = x;
+		this.Y = y;
+	}
 
+	public int getX() {
+		return X;
+	}
+
+	public int getY() {
+		return Y;
+	}
+
+	public void setX(int x) {
+		X = x;
+	}
+
+	public void setY(int y) {
+		Y = y;
+	}
+	
+	
+	
 }
