490a9c7a1f0b1102bf85556c969ba5081ded5a0d
Saghi Kungurov <saghi325@gmail.com>
2019-11-25 16:36:04 +0200

    Revert "Merge branch 'master' of https://github.com/SagKun/Snake-Spiderman-Group"
    This reverts commit 102e820479671a3bbf9e24f10f145532c024f1dd, reversing
    changes made to 2c6c85054e5e6b4c5127bb65f7a4e291aa15dce0.

--- SnakeProject/src/Model/FruitType.java
+++ SnakeProject/src/Model/FruitType.java
@@ -1,6 +1,5 @@
 package Model;
 
 public enum FruitType {
-// possible type of fruit
-		Banana, Apple, Pear, GoldBanana, GoldApple, GoldPear;
+
 }
--- SnakeProject/src/Model/GameState.java
+++ SnakeProject/src/Model/GameState.java
@@ -1,7 +1,5 @@
 package Model;
 
 public enum GameState {
-	// possible states of the game
-	Running, Paused, Finished, Started;
-}
 
+}
