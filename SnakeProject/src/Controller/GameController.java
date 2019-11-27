ce06cf9d7143471aba6c41602f334cadd43e4129
Naor91 <naorm203@gmail.com>
2019-11-28 00:42:33 +0200

    Updated board and GameController

--- SnakeProject/src/Controller/GameController.java
+++ SnakeProject/src/Controller/GameController.java
@@ -1,5 +1,332 @@
 package Controller;
 
-public class GameController {
+import java.net.URISyntaxException;
 
+import Model.*;
+import View.MainView;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+public class GameController{
+
+	/**
+	 * Actual state of the game
+	 */
+	protected static GameState state;
+	/**
+	 * Boolean variables describing user input
+	 */
+	private boolean up, down, right, left, pause, resume, start;
+	/**
+	 *  Boolean block to prevent pressing keys too fast, so that the snake's head could turn around.
+		For example, when snake was moving left, pressing the up and right key very fast could just change the head's direction
+		to right, without changing the position in Y-axis, causing the head to hit the second part of it's body  
+	 */
+	private boolean keyActive;
+	/**
+	 * The movement in X and Y-axis
+	 */
+	private int dx, dy;
+	/**
+	 * Variable to control snake's speed
+	 */
+	private int speedConstraint;
+	/**
+	 * Variable to change snake's speed at point intervals
+	 */
+	private int speedPointsConstraint;
+	private Snake snake;
+	private BodyPart head;
+	private MainView view; 
+	private Board board;
+	/**
+	 * MediaPlayer object, controls the music played in game
+	 */
+	private MediaPlayer audio;
+	
+	public GameController() {
+		state = GameState.Started;
+		up = down = right = left = pause = resume = start = false;
+		view = new MainView();
+		snake = view.getSnake();
+		head = snake.getHead();
+		board = view.getBoard();
+		keyActive = true; 
+		try {
+			audio = new Sound().getAudio();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		resume();
+	}
+	
+	/**
+	 * The gameloop, handles user input, updates and renders the game
+	 */
+	private void resume(){
+		
+		 new AnimationTimer(){
+			 
+				int i=0;
+				@Override
+				public void handle(long now) {
+									
+					// when moving up
+					if(up && !down) {
+						
+						dy = -1;
+						dx = 0;
+					}
+					// when moving down
+					if(!up && down) {
+						
+						dy = 1 ;
+						dx = 0;
+					}
+					// when moving left
+					if(left && !right) {
+						
+						dy = 0;
+						dx = -1;
+					}
+					//when moving right
+					if(right && !left) {
+						dy = 0;
+						dx = 1;
+					}
+					// when game paused
+					if(pause && !resume) {
+						state = GameState.Paused;
+						view.render();
+						stop();
+					}
+					// when game resumed
+					if(resume && !pause) {
+						state = GameState.Running;
+						resume = false;
+					}
+					// when game started or restarted
+					if(start && (state == GameState.Finished || state == GameState.Started)) {
+						restart();
+						start = false;
+					}
+					// when game finished
+					if(state == GameState.Finished) {
+						stop();
+					}	
+					// when game is running, make movement
+					if(state == GameState.Running) {
+						if(i==speedConstraint) { // control the speed of snake
+							move(dx, dy);
+							keyActive = true; // unlock possibility to press another key after snake made it's move
+							i=0; // counter to slow down the snake
+						}
+						++i;
+					}
+				
+					update(); // updating the game parameters, positions, etc.
+					view.render(); // rendering the scene
+					movement(view.getScene()); // handling user key input on actual scene
+				}
+			}.start(); // starting the timer
+		
+	}
+	
+	/**
+	 * Restarting the game by setting basic parameters to their primary values
+	 */
+	private void restart() {
+		state = GameState.Running;
+		dx = dy = 0;
+		up = down = left = right = false;
+		speedConstraint = 3; 
+		speedPointsConstraint = 50;
+	}
+
+	/**
+	 * The update method
+	 */
+	private void update() {
+		
+		board.updateFruit(); // updates the state of fruits
+		//board.updateObstacles(); // updating the obstacles on board
+		board.checkEaten(); // check if a fruit has been eaten
+		board.updateScore(); // updating score(passing it to scoreView class to show it on screen)
+		if(board.checkCollision() == GameState.Finished) { // check if a collision occurred
+			state = GameState.Finished; // 
+		}
+		setSound(); // updating the sound
+		
+		// setting snake speed due to gathered points
+		if(speedConstraint > 2 && board.getScore() >= speedPointsConstraint)
+			speedConstraint = 2; 		   //snake will move faster
+		if((speedConstraint == 2) && (board.getScore() - speedPointsConstraint) >= 10) {
+			speedPointsConstraint += 30;  // next interval 30 points further
+			speedConstraint = 3; 	   	  // back to original speed
+		}
+	}
+	
+	/**
+	 * Method to handle pressed keys on scene given as argument
+	 * @param scene on which events are performed
+	 */
+	private void movement(Scene scene) {
+		
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+			public void	handle(KeyEvent e){
+		                 
+				switch(e.getCode()) {
+					case UP:
+						if(!down && keyActive && state == GameState.Running) {
+							up = true;
+							left = false;
+							right = false;
+							keyActive = false;
+						}
+						break;
+					case DOWN:
+						if(!up && keyActive && (left || right) && state == GameState.Running) {
+							down = true;
+							left = false;
+							right = false;
+							keyActive = false;
+						}
+						break;
+					case LEFT:
+						if(!right && keyActive && state == GameState.Running) {
+							left = true;
+							up = false;
+							down = false;
+							keyActive = false;
+						}
+						break;
+					case RIGHT:
+						if(!left && keyActive && state == GameState.Running) {	
+							right = true;
+							up = false;
+							down = false;
+							keyActive = false;
+						}
+						break;
+					case SPACE: // pause or resume game
+						if(state == GameState.Running || state == GameState.Paused) {
+							if(pause == false) {
+								pause = true;
+								resume = false;
+							}
+							else {
+								resume = true;
+								pause = false;
+								resume();
+							}
+						}
+						break;
+					case ENTER:{ // start or restart the game
+							if(state == GameState.Started)
+								start = true;
+							if(state == GameState.Finished) {
+								start = true;
+								resume();
+							}
+						}
+						break;
+					case ESCAPE: // exit program
+						System.exit(0);
+						break;
+					default:
+						break;
+				}	
+			}
+		});
+		
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			 @Override
+	         public void handle(KeyEvent event) {
+			 }
+		});
+	}
+	
+	
+	/**
+	 * Method to handle snake's position and movement on board
+	 * @param dx - movement in X-axis, 1 for right, -1 for left
+	 * @param dy - movement in Y-axis, 1 for down, -1 for up
+	 */
+	private void move(int dx, int dy) {
+		
+		if(dx != 0 || dy != 0) { // if snake is meant to move
+						
+			// temporary variables to hold BodyParts
+						BodyPart prev = new BodyPart(head.getX(), head.getY()), next = new BodyPart(head.getX(), head.getY());
+
+						// move head in X-axis
+						head.setX(head.getX()+(dx*GameObject.SIZE));
+						// move head in Y-axis
+						head.setY(head.getY()+(dy*GameObject.SIZE));
+
+						// Right
+						if(head.getX() > MainView.WIDTH) {
+							state = GameState.Finished;
+						}
+						// Left
+						else if(head.getX() < 0) {
+							state = GameState.Finished;
+						}			
+						//Up
+						else if(head.getY() < 0) {
+							state = GameState.Finished;
+						}
+						//Down
+						else if(head.getY() > MainView.HEIGHT) {
+							state = GameState.Finished;
+						}
+								
+						// moving the snake's body, each point gets the position of the one in front
+						for(int i = 1; i < snake.getSize(); ++i) {
+
+							next.setX( snake.getBodyPart(i).getX());
+							next.setY( snake.getBodyPart(i).getY());
+					
+							snake.getBodyPart(i).setX(prev.getX());
+							snake.getBodyPart(i).setY(prev.getY());
+							prev.setX(next.getX());
+							prev.setY(next.getY());
+						}
+					}		
+				}
+	
+	/**
+	 * Method responsible for music in game
+	 */
+	private void setSound() {
+		
+		if(state == GameState.Running)	
+			audio.play(); // play music or resume when it was paused, when game is running	
+		if(state == GameState.Paused)
+			audio.pause(); // pause music when game is paused
+		if(state == GameState.Finished)
+			audio.stop(); // stop the music when game is finished	
+	}
+	
+	
+	/**
+	 * Static method for returning the actual state of game for the Model and View classes
+	 * @return Actual state of the game
+	 */
+	public static GameState getState() {
+		return state;
+	}	
+	
+	/**
+	 * Returns the stage, pass it to Main class
+	 * @return The game stage
+	 */
+	public Stage getStage() {
+		return view.getStage();
+	}
 }
--- SnakeProject/src/Model/Board.java
+++ SnakeProject/src/Model/Board.java
@@ -1,7 +1,561 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import Controller.Controller;
+import View.MainView;
+import View.ScoreView;
+import javafx.animation.*;
+import javafx.util.Duration;
+
 public class Board {
+
+	/**
+	 * Number of GameObjects to store in X-axis
+	 */
+	private static final int BWIDTH = MainView.WIDTH/GameObject.SIZE;
+	/**
+	 * Number of GameObjects to store in Y-axis
+	 */
+	private static final int BHEIGHT = MainView.HEIGHT/GameObject.SIZE;
+	/**
+	 * List of Apple fruits
+	 */
+	private ArrayList<Fruit> aFruits,bFruits,pFruits ;
 	
+	/**
+	 * Super fruit object
+	 */
+	private GoldenFruit sFruit;
+	/**
+	 * List of obstacles
+	 */
+	private ArrayList<Obstacle> obstacles; // List of obstacles
+	/**
+	 * Score value
+	 */
+	private int score, highscore, aFruitsEaten, bFruitsEaten, pFruitsEaten;
+	/**
+	 * Snake object
+	 */
+	private Snake snake; 
+	/**
+	 * Snake's head
+	 */
+	private BodyPart head; 
+	/**
+	 * Random number for generating points to place objects on them
+	 */
+	Random rand; 
+	/**
+	 * State of the game
+	 */
+	private GameState state;
+	/**
+	 *  Object of ScoreView to exchange informations about actual score
+	 */
+	private ScoreView scoreView; 
+	/**
+	 * Boolean value that tells if it's time to set a new super fruit on the board
+	 */
+	private boolean isSuper;
+	/**
+	 * Player start life
+	 */
+	private int life; 
+	
+	/**
+	 * Boolean value set true if snake is in super mode after eating the super fruit
+	 */
+	private boolean superState;
+	
+	/**
+	 * Timers for super fruit and it's effect
+	 */
+	private Timeline timeSuper, timeSFruit;
+	
+	/**
+	 * Default constructor of board class to initialize starting variables
+	 */
+	public Board() { 
+		
+		scoreView = new ScoreView();
+		aFruits = new ArrayList<>();
+		bFruits = new ArrayList<>();
+		pFruits = new ArrayList<>();
+		obstacles = new ArrayList<>();
+		score = aFruitsEaten = bFruitsEaten = pFruitsEaten = 0;
+		snake = new Snake();
+		rand = new Random();
+		head = snake.getHead();
+		state = GameState.Started;
+		sFruit = null;
+		isSuper = false;
+		superState = false;
+		life = 3;
+		
+	}
+	
+	/**
+	 * Method for updating fruits on board
+	 */
+	public void updateFruit() {
+		
+		int foodX = 0, foodY = 0; // foodX, foodY - coordinates for normal fruit
+		int []place; // place on board, will hold X and Y
+		
+		if(aFruits.size() < 1) { // if there's no fruit			
+			if(aFruitsEaten % 5 == 0 && aFruitsEaten != 0 ) { // adds super fruit
+				place = placeFruit();
+				foodX = place[0];
+				foodY = place[1];
+				//TODO ADD GoldApple
+				addFruit(foodX, foodY, "Apple");
+			}			
+			else {
+				place = placeFruit();
+				foodX = place[0];
+				foodY = place[1];
+				addFruit(foodX, foodY, "Apple");
+			}
+		}
+		if(bFruits.size() < 1) { // if there's no fruit
+			if(bFruitsEaten % 5 == 0 && bFruitsEaten != 0) { // adds super fruit
+				place = placeFruit();
+				foodX = place[0];
+				foodY = place[1];
+				//TODO ADD GoldBanana
+				addFruit(foodX, foodY, "Banana");
+			}			
+			else {
+				place = placeFruit();
+				foodX = place[0];
+				foodY = place[1];
+				addFruit(foodX, foodY, "Banana");
+			}	
+		}
+		if(pFruits.size() < 1) { // if there's no fruit
+			
+			if(pFruitsEaten % 5 == 0 && pFruitsEaten != 0) { // adds super fruit
+				place = placeFruit();
+				foodX = place[0];
+				foodY = place[1];
+				//TODO ADD GoldPear
+				addFruit(foodX, foodY, "Pear");
+			}			
+			else {
+				place = placeFruit();
+				foodX = place[0];
+				foodY = place[1];
+				addFruit(foodX, foodY, "Pear");
+			}
+		}
+	}
+	
+	/**
+	 * Method to place a fruit on the board
+	 * @return Returns point(X,Y) on board
+	 */
+	private int[] placeFruit() {
+		
+		int []point = new int[2];
+		
+		int helpX, helpY, foodX = 0, foodY = 0;
+		boolean helpS, helpO;	// for Snake and Obstacles
+		boolean collision = true;
 
+		while(collision) {
+				
+			helpS = helpO = false;
+			foodX = (rand.nextInt(BWIDTH)*GameObject.SIZE)+GameObject.SIZE/2;
+			foodY = (rand.nextInt(BHEIGHT)*GameObject.SIZE)+GameObject.SIZE/2;
+				
+			for(int i = 0; i < snake.getSize(); ++i) {
+					
+				helpX = snake.getBodyPart(i).getX();
+				helpY = snake.getBodyPart(i).getY();
+	
+				if(helpX == foodX && helpY == foodY) {
+					break;
+				}
+						
+				if(i == snake.getSize() - 1) {
+					helpS = true;
+				}
+			}
+					
+			if(helpS) {
+				
+				if(obstacles.size() == 0) {
+					helpO = true;
+				}
+				else {
+					
+					for(int i = 0; i < obstacles.size(); ++i) {
+					
+						helpX = obstacles.get(i).getX();
+						helpY = obstacles.get(i).getY();
+			
+						if(foodX == helpX && foodY == helpY) {
+							break;
+						}
+								
+						if(i == obstacles.size() - 1) {
+							helpO = true;
+						}
+					}
+				}
+				if(helpO) {	
+					collision = false;
+				}	
+			}		
+		}
+		point[0] = foodX;
+		point[1] = foodY;
+		return point;	
+	}
+		
+	/**
+	 * Method to check if an collision occurred, either of the snake head with it's body or with an any borders
+	 * @return Returns the finished state of game
+	 */
+	public GameState checkCollision() {
+		
+		int headX, headY, helpX, helpY;
+		
+		headX = head.getX();
+		headY = head.getY();
+		
+		// checks if snake hit itself
+		for(int i = 1; i < snake.getSize(); ++i) {
+			
+			helpX = snake.getBodyPart(i).getX();
+			helpY = snake.getBodyPart(i).getY();
+			
+			if(helpX == headX && helpY == headY) {
+				life--;
+				if (life > 0) { 
+					semiReset();
+					return GameState.Finished;
+						}
+				else {
+					highscore = score;
+					totalReset();
+					return GameState.Finished;	
+					}
+				
+			}
+		}
+		
+		// Checks if the snake has hit the board borders
+				if(headX > MainView.WIDTH || headX < 0 ) {
+					life--;
+					if (life > 0)
+						semiReset();
+					else {
+						highscore = score;
+						totalReset();
+						return GameState.Finished;	
+						}					
+				}
+				else if (headY < 0 || headY > MainView.HEIGHT ) {
+					life--;
+					if (life > 0)
+						semiReset();
+					else {
+						highscore = score;
+						totalReset();
+						return GameState.Finished;
+					}
+					
+				}	
+						
+				return Controller.getState();
+			
+	}
+	
+	/**
+	 * Method to check if snake ate a fruit
+	 */
+	public void checkEaten() {
+		
+		int headX, headY, foodX, foodY;
+		headX = head.getX();
+		headY = head.getY();
+		// checks if it's the super fruit
+		if(isSuper) {
+		
+			if(sFruit.getX() == headX && sFruit.getY() == headY) {
+				removeSuperFruit();
+				//++fruitsEaten;
+				score+=3;
+				superState = true;
+				timeSFruit.stop();
+				if(timeSuper != null) {
+					timeSuper.stop();
+				}
+				timeSuper = new Timeline(new KeyFrame(Duration.millis(GoldenFruit.SUPER_STATE_TIME), lambda->superState=false));
+				timeSuper.play();
+				return;
+			}
+		}
+		// check for a aFruit on board 	
+	for(int i = 0; i < aFruits.size(); ++i){
+					
+		foodX = aFruits.get(i).getX();
+		foodY = aFruits.get(i).getY();
+					
+		if(foodX == headX && foodY == headY) {
+			System.out.println("Apple Is Eaten");		
+			removeFruit(i,"Apple");
+			addLength(); // adds body part to snake
+			++aFruitsEaten;
+			System.out.println("****");
+			System.out.println("number of Apple " + aFruitsEaten);
+			System.out.println("****");
+				
+			if(superState) 
+				score+=2;
+			else
+				++score;
+				
+			//addObstacle = false;
+					}
+				}
+		// check for a bFruit on board 	
+		for(int i = 0; i < bFruits.size(); ++i){
+					
+			foodX = bFruits.get(i).getX();
+			foodY = bFruits.get(i).getY();
+					
+			if(foodX == headX && foodY == headY) {
+			System.out.println("Banana Is Eaten");			
+			removeFruit(i,"Banana");
+			addLength(); // adds body part to snake
+			++bFruitsEaten;
+			System.out.println("****");
+			System.out.println("number of Banana " + bFruitsEaten);
+			System.out.println("****");
+				
+			if(superState) 
+				score+=2;
+			else
+				++score;
+				
+			//addObstacle = false;
+					}
+				}
 
+		// check for a pFruit on board 	
+		for(int i = 0; i < pFruits.size(); ++i){
+					
+			foodX = pFruits.get(i).getX();
+			foodY = pFruits.get(i).getY();
+					
+			if(foodX == headX && foodY == headY) {
+				System.out.println("Pear Is Eaten");		
+				removeFruit(i,"Pear");
+				addLength(); // adds body part to snake
+				++pFruitsEaten;
+				System.out.println("****");
+				System.out.println("number of Pear " + pFruitsEaten);
+				System.out.println("****");
+				
+				if(superState) 
+					score+=2;
+				else
+					++score;
+				
+				//addObstacle = false;
+					}
+				}
+		}
+		
+	/**
+	 * Method to generate a new fruit in the game(2 if it's time for the super-fruit)
+	 * @param foodX X coordinate of normal fruit
+	 * @param foodY	Y coordinate of normal fruit
+	 * @param sFoodX X coordinate of super fruit(-1 by default)
+	 * @param sFoodY Y coordinate of super fruit(-1 by default)
+	 */
+	public void addFruit(int foodX, int foodY, String type) {
+		
+		switch (type) {
+		case "Apple":
+			System.out.println("New Apple add to the aFruits list");
+			aFruits.add(new Fruit(foodX, foodY)); // add new fruit to fruit array		
+			break;
+		case "Banana":
+			System.out.println("New Banana add to the bFruits list");
+			bFruits.add(new Fruit(foodX, foodY)); // add new fruit to fruit array
+			break;
+		case "Pear":
+			System.out.println("New Pear add to the pFruits list");
+			pFruits.add(new Fruit(foodX, foodY)); // add new fruit to fruit array	
+			break;
+		case "Gold":
+			//TODO Do something special
+			break;
+		default:
+			System.out.println(":)");
+			break;
+		}
+		
+	}
+	
+	/**
+	 * Method to remove a normal fruit from the board, after being eaten by snake
+	 * @param i Position of the fruit in array list
+	 */
+	public void removeFruit(int i, String type) {
+		
+		switch (type) {
+		case "Apple":
+				aFruits.remove(i);
+			break;
+		case "Banana":
+				bFruits.remove(i);			
+			break;
+		case "Pear":
+				pFruits.remove(i);
+			break;
+		
+		default:
+			System.out.println(":)");
+			break;
+		}
+		
+	}
+	
+	/**
+	 * Method to remove super fruit(make it a null value) 
+	 */
+	public void removeSuperFruit() {
+		isSuper = false;
+		sFruit = null;
+	}
+	
+	/**
+	 * Method for calling another method from ScoreView to add a point to the score
+	 */
+	public void updateScore() {
+		scoreView.addScore(score);
+	}
+	
+	/**
+	 * Add new part to snake's body after eating a fruit
+	 */
+	public void addLength() {
+		BodyPart b1 = snake.getBodyPart(snake.getSize()-1), b2 = snake.getBodyPart(snake.getSize()-2);
+		if(b1.getX() > b2.getX())
+			snake.addBodyPart(b1.getX()+GameObject.SIZE, b1.getY());
+		else if(b1.getX() < b2.getX())
+			snake.addBodyPart(b1.getX()-GameObject.SIZE, b1.getY());
+		else if(b1.getY() >= b2.getY())
+			snake.addBodyPart(b1.getX(), b1.getY()+GameObject.SIZE);
+		else if(b1.getY() >= b2.getY())
+			snake.addBodyPart(b1.getX(), b1.getY()-GameObject.SIZE);
+	}
+	
+	
+	/**
+	 * Resets basic values of the game after lose
+	 */
+	public void semiReset() {
+		snake.setStart();
+		aFruits.clear();
+		bFruits.clear();
+		pFruits.clear();
+		aFruitsEaten = 0;
+		superState = false;
+	}
+	
+	/**
+	 * Resets basic values of the game after game over
+	 */
+	public void totalReset() {
+		snake.setStart();		
+		aFruits.clear();
+		bFruits.clear();
+		pFruits.clear();
+		score = aFruitsEaten = 0;
+		superState = false;
+		removeSuperFruit();
+		life = 3;
+	}
+		
+	public ArrayList<Fruit> getaFruits(){
+		return aFruits;
+	}
+	public ArrayList<Fruit> getbFruits(){
+		return bFruits;
+	}
+	public ArrayList<Fruit> getpFruits(){
+		return pFruits;
+	}
+	
+	/**
+	 * Returns the super fruit
+	 * @return Super fruit object
+	 */
+	public GoldenFruit getSuperFruit() {
+		return sFruit;
+	}
+	
+	/**
+	 * Returns obstacles
+	 * @return Array with obstacles
+	 */
+	public ArrayList<Obstacle> getObstacles(){
+		return obstacles;
+	}
+	
+	/**
+	 * Returns the snake
+	 * @return Snake object
+	 */
+	public Snake getSnake() {
+		return snake;
+	}
+	
+	/**
+	 * Returns the object representing ScoreView in Board class
+	 * @return The ScoreView object
+	 */
+	public ScoreView getScoreView() {
+		return scoreView;
+	}
+	
+	/**
+	 * Returns the actual score
+	 * @return Value of score
+	 */
+	public int getScore() {
+		return score;
+	}
+	
+	/**
+	 * Returns the highscore when game finished
+	 * @return Value of final score
+	 */
+	public int getHighScore() {
+		return highscore;
+	}
+	
+	/**
+	 * Returns the actual state of the game
+	 * @return Value of GameState
+	 */
+	public GameState getState() {
+		return state;
+	}
+	
+	/**
+	 * Returns true if snake is in super state or false if not
+	 * @return Boolean true or false
+	 */
+	public boolean getSuperState() {
+		return superState;
+	}
 }
