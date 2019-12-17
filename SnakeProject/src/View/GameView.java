bc10287242324a7369c279598ff180ceb785b211
oryagol <oryagol@gmail.com>
2019-12-17 09:52:39 +0200

    fxml of gamview

--- SnakeProject/src/View/GameView.java
+++ SnakeProject/src/View/GameView.java
@@ -2,12 +2,35 @@ package View;
 
 import Controller.GameController;
 import Model.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 public class GameView {
 
 	private Board board;
 
 	private Snake snake;
+	
+    @FXML
+    private GridPane matrix;
+
+    @FXML
+    private ImageView life2;
+
+    @FXML
+    private ImageView life3;
+
+    @FXML
+    private Label life1;
+
+    @FXML
+    private TextField scoreField;
+
+    @FXML
+    private TextField extraLifeField;
 
 	public static final int width = 500;
 	public static final int height = 500;
