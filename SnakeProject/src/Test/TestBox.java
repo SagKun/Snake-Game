eac0d52484be1cac0edfbe98c4d74c30b1ec34e0
oryagol <oryagol@gmail.com>
2019-12-23 21:55:36 +0200

    fix testbox

--- SnakeProject/src/Test/TestBox.java
+++ SnakeProject/src/Test/TestBox.java
@@ -1,12 +1,12 @@
 package Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
 
 import Model.Board;
 import Model.FoodFactory;
@@ -18,12 +18,45 @@ import Model.SysData;
 
 public class TestBox {
 	
-	public static FoodType type = FoodType.Banana;
+	public static FoodType type;
+	public static int collisionX;
+	public static int collisionY;
+	public static int checkEatenX;
+	public static int checkEatenY;
+	
+	
+	@org.junit.jupiter.api.BeforeAll
+	public static void prepare() {
+		Random rand = new Random();
+		int pick = rand.nextInt(4);
+		switch(pick) {
+		case 0:
+			type = FoodType.Apple;
+			break;
+		case 1:
+			type = FoodType.Banana;
+			break;
+		case 2:
+			type = FoodType.Pear;
+			break;
+		case 3:
+			type = FoodType.Mouse;
+			break;
+		default:
+			type = FoodType.Apple;
+		}
+		collisionX = -1;
+		collisionY = 5;
+		checkEatenX = 300;
+		checkEatenY = 100;
+		
+	}
 
 	//checks if the factory creats object successfully and the object values are correct
 	@Test
 	public void testSetObjects() {
 		FoodFactory factory = new FoodFactory();
+		System.out.println(type);
 		SnakeFood obj = factory.getFood(type, 0, 0);
 		boolean result = false;
 		if(type.equals(FoodType.Apple) && obj.getPoints() == 10 && obj.getSecondsBuffer() == 5 &&
@@ -49,21 +82,17 @@ public class TestBox {
 	@Test
 	public void testShowSavedPlayers() {
 		SysData.loadHighScores();
-		boolean result = false;
-		if(!SysData.highScores.isEmpty())
-			result = true;
-		Assert.assertTrue(result);
+		Assert.assertFalse(SysData.highScores.isEmpty());
 	}
 	
 	//checks what happens when the snake collide with the borders
 	@Test
 	public void testCheckCollisionWithBorders() {
 		Board board = new Board();
-		board.getSnake().getHead().setX(-1);
-		board.getSnake().getHead().setY(5);
+		board.getSnake().getHead().setX(collisionX);
+		board.getSnake().getHead().setY(collisionY);
 		GameState state = board.checkCollision();
 		boolean result = false;
-		System.out.println(state);
 		if(state.equals(GameState.Finished))
 			result = true;
 		Assert.assertTrue(result);
@@ -76,12 +105,12 @@ public class TestBox {
 		Board board = new Board();
 		int prevScore = board.getScore();
 		int prevSnakeLength = board.getSnake().getSize();
-		board.getSnake().getHead().setX(300);
-		board.getSnake().getHead().setY(100);
+		board.getSnake().getHead().setX(checkEatenX);
+		board.getSnake().getHead().setY(checkEatenY);
 		board.initializeObjects();
 		SnakeFood prevFood = board.getObjectList().get(0);
-		board.getObjectList().get(0).setX(300);
-		board.getObjectList().get(0).setY(100);
+		board.getObjectList().get(0).setX(checkEatenX);
+		board.getObjectList().get(0).setY(checkEatenY);
 		board.checkEaten();
 		boolean result = false;
 		if(board.getScore() == prevScore+prevFood.getPoints() &&
@@ -97,12 +126,8 @@ public class TestBox {
 		ArrayList<Question> testQuestionDB = new ArrayList<Question>();
 		SysData.readQuestions();
 		testQuestionDB = SysData.questionsDB;
-		boolean result = false;
-		if(!testQuestionDB.toString().equals("[]"))
-			result = true;
-		Assert.assertTrue(result);
+		Assert.assertFalse(testQuestionDB.isEmpty());
 	}
-	
 	
 
 }
