9397b70f669ee5b88ae49597cdf364798b4c4d0c
eranrot12 <eranrot12@gmail.com>
2020-01-02 12:30:06 +0200

    Revert "GamePaused fixed"
    This reverts commit 0acbd0d83d17aff15db3bc7e9f2c87793c1a2eb9.

--- SnakeProject/src/View/GamePaused.java
+++ SnakeProject/src/View/GamePaused.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 public class GamePaused {
 
@@ -48,9 +47,7 @@ public class GamePaused {
 
     @FXML
     void resumeGameBtn(ActionEvent event) {
-    	Stage stage = (Stage)this.returnToGameBtn.getScene().getWindow();
-    	
-    		stage.close();
-    		
+
     }
+
 }
